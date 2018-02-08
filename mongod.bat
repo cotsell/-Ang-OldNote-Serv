dfbde597581945d5855f3994edfa8bce92561fb9
cotsell <cotsell@gmail.com>
2018-02-08 19:45:45 +0900

    소소한 수정.

--- mongod.bat
+++ mongod.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe" --dbpath=C:\Snowydayjy\AngularWorkspace\Project\Server\dbStore
+"C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe" --dbpath=C:\Snowydayjy\AngularWorkspace\ProjectNote\Server\dbStore
